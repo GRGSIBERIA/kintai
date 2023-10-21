e5a2566beba56f7f08ddb4cb901db5e818b586e1
grgsiberia <nanashi4129@gmail.com>
2023-10-21 15:19:33 +0900

    ビルド設定を変える

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@
 for file in ./seeder/*
 do
     cobc -x -F ${file} -o ./bin/seeder/$(basename ${file} .cob)
-    ./bin/seeder/$(basename ${file} .cob)
+done
+
+for file in ./src/*
+do
+    cobc -x -F ${file} -o ./bin/$(basename ${file} .cob)
 done
