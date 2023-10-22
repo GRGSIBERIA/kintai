10cc766bcd5735dbce7ee9350438658e486e50d1
grgsiberia <nanashi4129@gmail.com>
2023-10-22 15:20:17 +0900

    基本的な部分まで書く

---
+++ src/stamp-add.cob
@@ -0,0 +1,113 @@
+identification division.
+program-id. kintai-stamp-add.
+
+environment division.
+input-output section.
+       file-control.
+           select user-file assign to "./dat/stamp.dat"
+               organization relative
+               access mode sequential
+               relative key user-key
+               status user-status.
+           select stamp-file assign to "./dat/stamp.dat"
+               organization relative
+               access mode sequential
+               relative key stamp-key
+               status stamp-status.
+           select log-file assign to "./dat/log.dat"
+               organization relative
+               access mode sequential
+               status log-status.
+data division.
+file section.
+       fd user-file.
+           01 Fuser-rec.
+               03 Fuser-id pic 9(7).
+               03 Fusername pic X(64).
+               03 Ffirstname pic N(32) usage national.
+               03 Flastname pic N(32).
+               03 Fpswd pic X(20).
+               03 Fgender pic 9(2).
+               03 Faddress pic N(70).
+               03 Femail pic X(254).
+               03 Fphone-number pic X(14).
+               03 Froll pic 9(2).
+               03 Fjoin-date pic X(21).
+       fd stamp-file.
+           01 Fstamp-rec.
+               03 Fstamp-id pic 9(12).
+               03 Fuser-id pic 9(7).
+               03 Fstamp-datetime pic X(21).
+               03 Fstatusid pic 9.
+       fd log-file.
+           01 Flog-rec.
+               03 log-timestamp pic X(21).
+               03 log-comments pic X(128).
+working-storage section.
+       01 key-rec.
+           03 user-key pic 9(7).
+           03 stamp-key pic 9(12)
+       01 status-rec.
+           03 user-status pic XX.
+           03 stamp-status pic XX.
+       01 auth-rec.
+           03 auth-username pic X(64).
+           03 auth-password pic X(20).
+           03 miss-count pic 9 values 0.
+
+procedure division.
+       display "勤怠管理システム".
+       display "勤怠追加モード".
+
+authenticate-username.
+       display "ユーザ認証を行います".
+       display "ユーザ名を入力してください".
+       accept auth-username.
+
+       open input user-file.
+       perform until user-status not = "00"
+           if auth-username = Fusername then
+               close user-file
+               go to authenticate-password
+           end-if
+       end-perform.
+
+       display "ユーザが見つかりません".
+       display "もう一度入力してください".
+       go to authenticate-username.
+
+authenticate-password.
+       display "パスワードを入力してください".
+       accept auth-password.
+
+       if miss-count >= 3 then
+           display "ユーザ認証に3回失敗しました"
+           display "プログラムを強制終了します"
+           go to authenticate-logging
+       end-if.
+
+       if auth-password = Fpswd then
+           go to stamp-procedure
+       end-if.
+
+       display "パスワードの認証に失敗しました".
+       display "もう一度入力してください".
+       add 1 to miss-count.
+
+       go to authenticate-password.
+
+authenticate-logging.
+       open extend log-file.
+       move function current-date to log-timestamp.
+       string
+           "[ERRO] " delimited by size
+           function trim(auth-username) delimited by size
+           " failed authentication 3 times."
+           into log-comments
+       end-string.
+       write Flog-rec.
+       close log-file.
+       stop run.
+
+stamp-procedure.
+       
